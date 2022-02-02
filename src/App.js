a8ff2ee05b7feb013c87077c6f8b75f656649a1c
Vladyslav Gusiev <vladyslav.gusiev@gmail.com>
2022-02-02 20:00:10 +0100

    added all basic items and styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import TestButton from './components/TestButton';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+        <h1 className='header'>NUMBER FACTS</h1>
+        <h2 className='main-text'> _________________________________________________________________ </h2>
+
+        <div className='main-body'>
+          <h2 className='main-text'>EXAMPLE NUMBERS:</h2>
+          <TestButton number={"1"}/>
+          <TestButton number={"2"}/>
+          <TestButton number={"3"}/>
+          <TestButton number={"4"}/>
+          <TestButton number={"5"}/>
+
+          <h3 className='main-text'>TRY ANY NUMER!</h3>
+
+          <input type="number" id="number-to-proccess" name="number-to-process" min="0" max="9999" />
+
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
